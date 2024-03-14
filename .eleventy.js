0d57ae5c2aa8a36b6fe8aafd2403970bdffbdef9
ORIGIN INVESTMENTS <origininvestments@ORIGINs-MacBook-Pro.local>
2024-03-14 13:43:53 -0500

    prefix

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass);
 
   return {
-    // pathPrefix: "/subdirectory/", //If using a base path for your site
+    pathPrefix: "/pose/", //If using a base path for your site
     dir: {
       input: "src",
       output: "public",
